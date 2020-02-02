d9f981cdfdd6490c962d00cab964ea71aabc6bd1
DenShlk <52719698+DenShlk@users.noreply.github.com>
2020-02-02 11:52:09 +0300

    +адресная срока в браузере

--- app/src/main/java/com/example/refreshit/BrowserActivity.java
+++ app/src/main/java/com/example/refreshit/BrowserActivity.java
@@ -2,9 +2,12 @@ package com.example.refreshit;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +15,11 @@ import androidx.appcompat.app.AppCompatActivity;
 public class BrowserActivity extends AppCompatActivity {
 
 	static final private int ADD_PAGE = 1;
+	static final private String INITAL_URL = "http://google.com";
 
 	WebView webView;
 	Button confirmButton;
+	EditText urlInput;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +27,27 @@ public class BrowserActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_browser);
 
 		confirmButton = findViewById(R.id.confirm_button);
+
 		webView = findViewById(R.id.webView);
 
 		webView.getSettings().setJavaScriptEnabled(true);
-		webView.loadUrl("http://google.com");
+		webView.loadUrl(INITAL_URL);
 		webView.setWebViewClient(new MyWebViewClient());
+
+		urlInput = findViewById(R.id.url_input);
+		urlInput.setText(INITAL_URL);
+		urlInput.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				webView.loadUrl(String.valueOf(s));
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {}
+		});
 	}
 
 	@Override
